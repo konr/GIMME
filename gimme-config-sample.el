56c5f144242bccda10a75207d72684dcf33774f1
Konrad Scorciapino <konrad@scorciapino.com>
2010-07-18 06:45:10 -0300

    test

--- gimme-config-sample.el
+++ gimme-config-sample.el
@@ -2,7 +2,7 @@
                                      (apply #'propertize (decode-coding-string (format "%s\n" %title) 'utf-8)
                                             (plist-put plist 'font-lock-face `(:foreground ,(color-for %album))))
                                    (apply #'propertize (decode-coding-string (format "* %s\n" %title) 'utf-8)
-                                          (plist-put plist 'font-lock-face `(:foreground ,(color-for %album)))) )
+                                          (plist-put plist 'font-lock-face `(:foreground ,(color-for %album) :weight bold))) )
                                  (apply #'propertize (decode-coding-string (format "%s\n" %title) 'utf-8)
                                         (plist-put plist 'font-lock-face `(:foreground ,(color-for %album))))
                                  (apply #'propertize (decode-coding-string (format "%s > %s\n" %artist %title) 'utf-8)
@@ -10,11 +10,5 @@
 
 (defvar gimme-sort-criteria '((artist album tracknr) (artist title) (title)))
 
-
-(comment if (null %starred)
-         (apply #'propertize (decode-coding-string (format "%s\n" %title) 'utf-8)
-                (plist-put plist 'font-lock-face `(:foreground ,(color-for %album))))
-         (apply #'propertize (decode-coding-string (format "* %s\n" %title) 'utf-8)
-                (plist-put plist 'font-lock-face `(:foreground ,(color-for %album)))) )
 
 (provide 'gimme-config)
