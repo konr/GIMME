9303c19d2f0192f15463c5157c57212eb24fe9f6
Konrad Scorciapino <konr@konr.mobi>
2011-05-05 05:00:57 -0300

    removed a file

--- observer.el
+++
@@ -1,8 +0,0 @@
-(provide 'observer)
-(defvar observer-plist nil)
-
-(defun observer-var-add (name &optional initial-value)
-  (setq observer-plist (plist-put observer-plist name initial-value)))
-
-(defun change-observed-data (data value)
-  )
