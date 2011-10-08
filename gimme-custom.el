3329fc4ef0354f3e5f60379039950188ef8fa71a
Rémi Vanicat <vanicat@debian.org>
2011-10-08 13:37:41 +0200

    Fix doc for a customize for M-x customize-group to run.

--- gimme-custom.el
+++ gimme-custom.el
@@ -76,7 +76,6 @@
   :type 'integer)
 
 (defcustom gimme-presets
-  "FIXME: To be done"
   '("1965" (-20 -16 -7 -4 -4 -4 -7 -7 3 3 -2 -4 4 1 1 -4 -6 -12)
     "Air" (0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 3 2)
     "Brittle" (-12 -10 -9 -8 -7 -6 -5 -3 -2 -2 -2 -2 -1 1 4 4 1 0)
@@ -97,6 +96,7 @@
     "Shimmer" (0 0 0 -2 -2 -7 -5 0 0 0 0 0 4 1 3 3 4 0)
     "Soft Bass" (3 5 4 0 -13 -7 -5 -5 -1 2 5 1 -1 -1 -2 -7 -9 -14)
     "Strings" (-3 -4 -4 -5 -5 -4 -4 -3 -2 -2 -2 -2 -1 2 3 0 -2 -2))
+  "FIXME: To be done"
   :group 'gimme
   :type '(repeat sexp))
 
